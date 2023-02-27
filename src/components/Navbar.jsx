efb4980ccada1f92466c05a03bd80bd64eacef29
ORayy <asare.oanane@gmail.com>
2023-02-27 01:35:27 +0000

    created initial page layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,20 @@
+import About from "../pages/About"
+import Help from "../pages/Help"
+import Create from "../pages/Create"
 
 export default function Navbar() {
   return (
-    <nav className="navbar">Nav bar</nav>
+    <nav className="navbar">
+      <nav className="nav-items">
+        <nav className="nav-logo">LoGO</nav>
+        <nav className="nav-links">
+          <ul className="nav-links-items">
+            <li><a href={<About />}>About</a></li>
+            <li><a href={<Help />}>Help</a></li>
+            <li><a href={<Create />}>Create</a></li>
+          </ul>
+        </nav>
+      </nav>
+    </nav>
   )
 }
