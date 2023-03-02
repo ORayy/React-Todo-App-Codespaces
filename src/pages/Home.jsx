c963dd10ca43731655ae04d04792e086abec6a1a
ORayy <asare.oanane@gmail.com>
2023-03-02 14:04:40 +0000

    added stylinf for Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,8 @@
-// import React from 'react'
-// import Navbar from "../components/Navbar"
+import '../styles/home.css'
 
 export default function Home() {
   return (
-    <main>
+    <main className="home-main">
       <h1> Welcome</h1>
       <br />
 
