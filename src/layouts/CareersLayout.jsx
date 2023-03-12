59c897821b3c9046432d9d52f436937f7806c3e1
ORayy <asare.oanane@gmail.com>
2023-03-12 00:29:47 +0000

    created db.json file

--- src/layouts/CareersLayout.jsx
+++ src/layouts/CareersLayout.jsx
@@ -7,6 +7,7 @@ import '../styles/careers.css'
 export default function CareersLayout() {
   return (
     <div className="careers-main">
+      
       <section className="careers-section">
         <NavLink className='career-link' to='careers' >Careers</NavLink>
         <NavLink className='career-link' to='add' >Add</NavLink>
